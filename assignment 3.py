3ca4a2826ecfe5949f4d61e40275ff3492d4781d
Arti22kesar <arti83713@gmail.com>
2018-06-14 12:32:35 +0530

    assignment3 done

---
+++ assignment 3.py
@@ -0,0 +1,47 @@
+#Ques1.
+
+a=1,2,3
+l=[]
+l.append(a)
+print(l)
+
+
+#Ques2.
+
+a=[]
+b="google","apple","facebook","microsoft","tesla"
+a.append(b)
+print(a)
+
+#Ques3.
+
+a="google","apple","google","facebook","google"
+print(a.count("google"))
+
+
+#Ques4.
+
+a=[1,8,4,3,6]
+a.sort()
+print(a)
+
+
+#Ques5.
+
+a=[1,2,3,4]
+b=[5,6,7]
+c=a+b
+print(c)
+
+#Ques6.
+
+a=[1,2,3,4,5]
+a.pop()
+print(a)
+
+
+#Ques7.
+
+number =(1,2,3,4,5,6,7)
+print("total evens:",len([i for i in number if(i%2==0)]))
+print("total odds:",len([i for i in number if(i%2!=0)]))
